adca4e4d50a60d29844367e467c92bc951d6b2c9
vizzaana <aviza@pinetop.tech>
2023-10-31 21:23:24 +0700

    add conditional render in login

--- src/pages/SSOLogin/index.jsx
+++ src/pages/SSOLogin/index.jsx
@@ -1,10 +1,16 @@
-import { Card, Input, Button, Typography } from 'antd';
+import React, { useState } from 'react';
+import { Card, Input, Button, Typography, Switch } from 'antd';
 import { UserOutlined, KeyOutlined } from '@ant-design/icons';
 import './index.css'
 
 const { Text } = Typography;
 
+
 const SSOLogin = () =>{
+    const [isAdmin, setIsAdmin]=useState(false)
+    const AdminHandler =(checked)=>{
+        setIsAdmin(checked)
+    }
     return(
         <>
         <div style={{ background: '#B9F0FC', padding: '30px', height:'100vh' }}>
@@ -16,10 +22,22 @@ const SSOLogin = () =>{
                 top:'50%',
                 transform: 'translate(-50%,-50%)'
             }}>
-                <Input size="large" placeholder="Nomor Induk Siswa" prefix={<UserOutlined style={{marginRight:'10px', color:'#cfcfcf'}}/>} style={{marginBottom: '20px'}}/>
+                <div style={{display:'flex', justifyContent:'space-between', marginBottom:'20px'}}>
+                    Masuk sebagai admin ?
+                    <Switch checkedChildren='Ya' unCheckedChildren='Tidak' onChange={AdminHandler}/>
+                </div>
+                {isAdmin ?
+                    <Input size="large" placeholder="Username" prefix={<UserOutlined style={{marginRight:'10px', color:'#cfcfcf'}}/>} style={{marginBottom: '20px'}}/>
+                :
+                
+                    <Input size="large" placeholder="Nomor Induk Siswa" prefix={<UserOutlined style={{marginRight:'10px', color:'#cfcfcf'}}/>} style={{marginBottom: '20px'}}/>
+             
+                }
                 <Input size="large" placeholder="Kata Sandi" prefix={<KeyOutlined style={{marginRight:'10px', color:'#cfcfcf'}}/>} style={{marginBottom: '20px'}}/>
                 <Button style={{width:'100%', background:'#a0d911', color:'white'}}>Masuk</Button>
             </Card>
+            {isAdmin ?
+            null :
             <Text 
             style={{
                 position : 'relative',
@@ -30,6 +48,7 @@ const SSOLogin = () =>{
                 fontSize:'12px'
                 // transform: 'translate(-50%,-50%)'
             }}>*Catatan: Jika belum memiliki akun, tolong hubungi administrator</Text>
+            }
         </div>
         </>
     )
